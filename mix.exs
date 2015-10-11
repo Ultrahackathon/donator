1a70ba93861b5f0baa4b328da9c3a9d8a7e9c2a2
Antti Forsell <fosu@fosu.me>
2015-10-11 13:12:26 +0300

    Removes psql dependency

--- mix.exs
+++ mix.exs
@@ -32,7 +32,6 @@ defmodule Donator.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.0.3"},
      {:phoenix_ecto, "~> 1.1"},
-     {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0"}]
