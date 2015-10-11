c971cc2c33c8fbd0605220705e98c12968a86d50
Timo Sand <timo.j.sand@gmail.com>
2015-10-11 20:33:21 +0300

    WEB Adds webpack for building UI

---
+++ web/static/js/app.js
@@ -0,0 +1,8 @@
+// import React from "bower_components/react/react"
+// import DonatorApp from "./components/DonatorApp"
+//
+// React.render(
+//   <DonatorApp />,
+//   document.getElementById("donator")
+// );
+console.log("Hello from Webpack");
