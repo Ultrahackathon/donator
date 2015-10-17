1644c0ab829357a3faef5561e61635a76ee3c6c8
Timo Sand <timo.j.sand@gmail.com>
2015-10-18 00:27:51 +0300

    REACT Replaces deprecated call to React.render

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React from "react"
+import ReactDOM from "react-dom"
 import DonatorApp from "./components/DonatorApp"
 
-React.render(
+ReactDOM.render(
   <DonatorApp />,
   document.getElementById("donator")
 );
