021eff1465c1148e1634311c69132340af505014
Timo Sand <timo.j.sand@gmail.com>
2015-11-03 23:45:03 +0200

    CHECKIN Adds getting location from user

--- web/static/js/components/Check-in.jsx
+++ web/static/js/components/Check-in.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
 export default class CheckIn extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {geolocation: ['empty']}
+  }
+
+  componentWillMount() {
+    if ('geolocation' in navigator) {
+      this.setState({geolocation: ['notempty']})
+      navigator.geolocation.getCurrentPosition( (pos) => {
+        this.setState({geolocation: [pos.coords.latitude, pos.coords.longitude]})
+      })
+    } else {
+      this.setState({geolocation: ['foobarbaz']})
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.geolocation !== this.state.geolocation
+  }
+
+
   render() {
-    return <h2>This is the Check-in</h2>
+    return (
+      <div>
+        <h2>This is the Check-in</h2>
+        <p>Location: {this.state.geolocation.join(',')}</p>
+      </div>
+    )
   }
 }
