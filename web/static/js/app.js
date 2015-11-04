86713763c180a85aa9af7464698ca17a238fb767
Timo Sand <timo.j.sand@gmail.com>
2015-11-04 20:45:00 +0200

    REFACTOR Removes moved code

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -7,21 +7,7 @@ import Map from './components/Map'
 import CheckIn from './components/Check-in'
 import Leaderboard from './components/Leaderboard'
 import Profile from './components/Profile'
-import {Socket} from "./phoenix"
 
-function connect() {
-  let socket = new Socket("/socket", {params: {token: document.cookie.split('=')[1]}})
-  socket.connect()
-  let channel = socket.channel("actions", {token: document.cookie.split('=')[1]})
-
-  channel.on("locations:near", payload => {
-    console.log(payload)
-  })
-  channel.join()
-    .receive("ok", ({messages}) => channel.push('locations:near', {lat: 60.169308699999995, lng: 24.9292901}, 10000))
-    .receive("error", ({reason}) => console.log("failed join", reason) )
-    .receive("timeout", () => console.log("Networking issue. Still waiting...") )
-}
 render((
   <Router>
     <Route path="/" component={DonatorApp}>
