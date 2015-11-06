90ea477de1151e7325f95b984d0ce068f17ef14e
Niko Kauppila <niko.kauppila@iki.fi>
2015-11-06 23:35:49 +0200

    Add simple ui for leaderboard

--- web/static/js/components/Leaderboard.jsx
+++ web/static/js/components/Leaderboard.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
 
 export default class Leaderboard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      users: []
+    }
+
+    this.props.channel.on('leaderboard', payload => {
+      this.setState({users: payload.leaderboard})
+    })
+    this.props.channel.push('leaderboard')
+
+  }
+
   render() {
-    return <h2>This is the Leaderboard</h2>
+    return <div>
+      <h2>This is the Leaderboard</h2>
+      {this.state.users.map((user, index) => <LeaderboardRow key={index} user={user} position={index} />)}
+    </div>
+  }
+}
+
+export class LeaderboardRow extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <div>
+      <b>{this.props.user.name} {this.props.user.checkin_count} Checkins</b>
+    </div>
   }
 }
