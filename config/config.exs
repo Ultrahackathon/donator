593a365b88f995ccfc0750bd6e64b711e18b9aed
Niko Kauppila <niko.kauppila@iki.fi>
2015-11-06 22:09:32 +0200

    Environment config override default config

--- config/config.exs
+++ config/config.exs
@@ -19,10 +19,6 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
-
 # Configure phoenix generators
 config :phoenix, :generators,
   migration: true,
@@ -40,3 +36,7 @@ config :donator, :jwt,
 
 config :donator, :foursquare,
   endpoint: "https://api.foursquare.com/v2/venues/search?v=20151010"
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
