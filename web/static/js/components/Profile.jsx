6aa8053e5870567a5a907404bebd3172f2b5adb1
Niko Kauppila <niko.kauppila@iki.fi>
2015-11-07 05:37:58 +0200

    Add simple profile component

--- web/static/js/components/Profile.jsx
+++ web/static/js/components/Profile.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 
 export default class Profile extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      checkins: []
+    }
+  }
+
+  componentWillMount() {
+    this.props.channel.on('user', payload => {
+      this.setState(payload)
+    })
+    this.props.channel.push('user')
+  }
+
   render() {
-    return <h2>This is the Profile</h2>
+    return <div>
+      <h2>This is the Profile</h2>
+      <h3>{this.state.name} - {this.state.checkins.length} checkins</h3>
+      <p>{this.state.email}</p>
+    </div>
   }
 }
