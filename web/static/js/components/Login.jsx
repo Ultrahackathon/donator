a59f6c9f7d0727060045f0dbf390f7b4dfac3543
Timo Sand <timo.j.sand@gmail.com>
2015-11-08 00:58:54 +0200

    UI Updates icons

--- web/static/js/components/Login.jsx
+++ web/static/js/components/Login.jsx
@@ -7,9 +7,9 @@ export default class Login extends React.Component {
     if (!this.props.isAuthenticated) {
       logins = (
         <div id="logins">
-          <a href="/auth/github">Login with GitHub</a>&nbsp;
-          <a href="/auth/facebook">Login with Facebook</a>&nbsp;
-          <a href="/auth/google">Login with Google</a>
+          <a href="/auth/github" alt="Login with GitHub" className="icon"><span className="icon-github" /></a>&nbsp;
+          <a href="/auth/facebook" alt="Login with Facebook" className="icon"><span className="icon-facebook3" /></a>&nbsp;
+          <a href="/auth/google" alt="Login with Google" className="icon"><span className="icon-google" /></a>
         </div>
       )
     }
--- web/static/js/components/Navigation.jsx
+++ web/static/js/components/Navigation.jsx
@@ -7,11 +7,11 @@ export default class Navigation extends React.Component {
     return (
       <div className="nav-container">
         <nav>
-          <Link id="feed" to="/feed" activeClassName="active"><span className="icon-users" /></Link>&nbsp;
+          <Link id="feed" to="/feed" activeClassName="active"><span className="icon-users2" /></Link>&nbsp;
           <Link id="map" to="/map" alt="Map" activeClassName="active"><span className="icon-map" /></Link>&nbsp;
-          <Link id="checkin" to="/checkin" alt="Check-in" activeClassName="active"><span className="icon-checkin" /></Link>&nbsp;
+          <Link id="checkin" to="/checkin" alt="Check-in" activeClassName="active"><span className="icon-checkin2" /></Link>&nbsp;
           <Link id="leaderboard" to="/leaderboard" activeClassName="active"><span className="icon-list-ol" /></Link>&nbsp;
-          <Link id="profile" to="/profile" activeClassName="active"><span className="icon-user" /></Link>&nbsp;
+          <Link id="profile" to="/profile" activeClassName="active"><span className="icon-user2" /></Link>&nbsp;
         </nav>
       </div>
     )
