4afe3b35781bce85f74efcb4f8631cf2ad24a9b9
Timo Sand <timo.j.sand@gmail.com>
2015-11-08 14:39:23 +0200

    DEMO Remove other oath provider links

--- web/static/js/components/Login.jsx
+++ web/static/js/components/Login.jsx
@@ -7,8 +7,6 @@ export default class Login extends React.Component {
     if (!this.props.isAuthenticated) {
       logins = (
         <div id="logins">
-          <a href="/auth/github" alt="Login with GitHub" className="icon"><span className="icon-github" /></a>&nbsp;
-          <a href="/auth/facebook" alt="Login with Facebook" className="icon"><span className="icon-facebook3" /></a>&nbsp;
           <a href="/auth/google" alt="Login with Google" className="icon"><span className="icon-google" /></a>
         </div>
       )
